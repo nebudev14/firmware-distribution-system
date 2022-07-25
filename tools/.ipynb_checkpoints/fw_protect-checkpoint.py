457b02d9037a8d5321857d43237f4230bb2ea9c2
NebuDev14 <wyun2006@gmail.com>
2022-07-25 15:50:00 +0000

    sign

--- tools/.ipynb_checkpoints/fw_protect-checkpoint.py
+++ tools/.ipynb_checkpoints/fw_protect-checkpoint.py
@@ -4,9 +4,7 @@ Firmware Bundle-and-Protect Tool
 """
 import argparse
 import struct
-from Crypto.Cipher import AES
-from Crypto.PublicKey import ECC
-from Crypto.Signature import eddsa
+
 
 def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
@@ -22,30 +20,9 @@ def protect_firmware(infile, outfile, version, message):
     # Append firmware and message to metadata
     firmware_blob = metadata + firmware_and_message
 
-    # sign
-    key = ECC.import_key(open('secret_build_output.txt').read())
-    signer = eddsa.new(key, 'rfc8032')
-    
-    
-    # load secret key from file(first 16 bytes)
-    with open('secret_build_output.txt', 'rb') as secrets_file:
-        aes_key = secrets_file.read(16)
-    
-    # Create cipher object
-    cipher = AES.new(aes_key, AES.MODE_GCM)
-    # assert that version is an integer
-    cipher.update(version)
-    
-
-    nonce = cipher.nonce
-    
-    encrypted_firmware_blob, tag = cipher.encrypt_and_digest(firmware_blob)
-    
-    
-
     # Write firmware blob to outfile
     with open(outfile, 'wb+') as outfile:
-        outfile.write(encrypted_firmware_blob + tag + nonce)
+        outfile.write(firmware_blob)
 
 
 if __name__ == '__main__':
